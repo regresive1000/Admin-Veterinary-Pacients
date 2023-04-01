5ceb1995b973364d509ec6228c73eac5762b178f
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-04-01 10:24:33 -0300

    Se agrega el Proyecto

---
+++ backend/controllers/veterinarioController.js
@@ -0,0 +1,27 @@
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (req, res) => {
+    const {nombre, email, password} = req.body;
+
+    try{
+        //Guardar un nuevo Veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+
+        res.json({msj: 'Registrando usuario...'})
+    } catch (error){
+        console.log(error)
+    }
+
+    
+};
+
+const perfil = (req, res) => {
+    res.json({msg: "Mostrando perfil"})
+};
+
+
+export {
+    registrar,
+    perfil
+}
