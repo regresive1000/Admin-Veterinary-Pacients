fcb4008da4a1bbdc4a3dfe5da5516e48d140f994
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-04-24 01:02:07 -0300

    Update and create Paciente Model, Controller, Route

---
+++ backend/controllers/pacienteController.js
@@ -0,0 +1,22 @@
+import Paciente from '../models/Paciente.js'
+
+
+const agregarPaciente = async (req, res) => {
+    const paciente = new Paciente(req.body);
+    paciente.veterinario = req.veterinario._id;
+    try {
+        const pacienteGuardado = await paciente.save()
+        res.json(pacienteGuardado)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const obtenerPacientes = (req, res) => {
+
+}
+
+export {
+    agregarPaciente,
+    obtenerPacientes
+}
--- backend/helpers/generarJWT.js
+++ backend/helpers/generarJWT.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const generarJWT = (id) => {
+const generarJWT = (id) => { // ID de vet
 
     return jwt.sign({id}, process.env.JWT_SECRET, {
         expiresIn: "30d",
