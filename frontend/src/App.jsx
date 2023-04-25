7a70dda7c29ce60818f30fef99d3e78db75750d9
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-04-25 12:20:19 -0300

    fix dir problems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthLayout from './layout/AuthLayout';
+import Login from './paginas/Login';
+import Registrar from './paginas/Registrar';
+import OlvidePassword from './paginas/OlvidePassword';
+import ConfirmarCuenta from './paginas/ConfirmarCuenta';
 
 function App() {
   
 
   return (
-    <h1 className="font-bold text-2xl">Hola Vite</h1>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<Login />} /> /* Index hace que AuthLayout (que es la pagina principal), la convierte en la raiz */
+          <Route path="registrar" element={<Registrar />} /> /* Path son las extensiones de la raíz y las secciones a printear */
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/paginas/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+   return (
+     <>
+     <h2>Login</h2>
+     <div>1</div>
+     <div>2</div>
+     </>
+   )
+ }
+ 
+ export default Login;
