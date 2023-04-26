50647d10a9a0f6e7885e30c79f42f6b5d97838d8
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-04-25 23:19:04 -0300

    init login, register, forgot-password React

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,9 +1,121 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
 const Registrar = () => {
-    return (
-      <>
-          <h1>Desde Registrar</h1>
-      </>
-    )
+
+  const [ nombre, setNombre ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ repetirPassword, setRepetirPassword ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if([nombre, email, password, repetirPassword].includes('')) {
+      console.log('Hay campos vacios')
+      return;
+    }
+
+    if(password !== repetirPassword) {
+      console.log('Los passwords no son iguales');
+      return;
+    }
+
+    if(password.length < 8) {
+      console.log('El password es muy corto');
+      return;
+    }
+  }
+
+  return (
+    <>
+
+      <h1 className="text-indigo-600 font-black text-6xl">
+        Crea tu Cuenta y Administra tus
+        <span className="text-black"> Pacientes</span></h1>
+
+      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+        <form
+          
+          onSubmit={handleSubmit}
+        >
+
+          <div className="my-5">
+            <label htmlFor="" className=" text-gray-600 block text-xl font-bold">Nombre</label>
+            <input
+              type="text"
+              placeholder="Tu Nombre"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={nombre}
+              onChange={e => setNombre(e.target.value)}
+            />
+          </div>
+
+          <div className="my-5">
+            <label htmlFor="" className=" text-gray-600 block text-xl font-bold">Email</label>
+            <input
+              type="email"
+              placeholder="Email de Registro"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="my-5">
+
+            <label htmlFor="" className=" text-gray-600 block text-xl font-bold">Password</label>
+
+            <input
+              type="password"
+              placeholder="Tu contraseña"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="my-5">
+
+            <label htmlFor="" className=" text-gray-600 block text-xl font-bold">Repetir Password</label>
+
+            <input
+              type="password"
+              placeholder="Repite tu Password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={repetirPassword}
+              onChange={e => setRepetirPassword(e.target.value)}
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Crear Cuenta"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white  font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+          />
+
+        </form>
+
+        <nav className='mt-10 lg:flex lg:justify-between'>
+          <Link
+            to="/"
+            className='block text-center my-5 text-gray-500'
+          >
+            ¿Ya tienes una cuenta? Inicia Sesión
+          </Link>
+          <Link
+            to="/olvide-password"
+            className='block text-center my-5 text-gray-500'
+          >
+            Olvide mi Password
+          </Link>
+        </nav>
+      </div>
+
+      
+    </>
+  )
   }
   
   export default Registrar
