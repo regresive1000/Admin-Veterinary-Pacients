467a1246efe59aa5b3710b8352619d459f9d1949
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-05-04 10:26:15 -0300

    email sending config

--- backend/helpers/emailOlvidePassword.js
+++ backend/helpers/emailOlvidePassword.js
@@ -4,6 +4,7 @@ const emailOlvidePassword = async ({nombre, token, email}) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
--- backend/helpers/emailRegistro.js
+++ backend/helpers/emailRegistro.js
@@ -4,6 +4,7 @@ const emailRegistro = async ({ nombre, email, token}) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
